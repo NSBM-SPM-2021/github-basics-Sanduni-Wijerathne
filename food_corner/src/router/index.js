ae8cb43a3373e549428fecb90fe185b08e82e16c
Sanduni-Wijerathne <55197806+Sanduni-Wijerathne@users.noreply.github.com>
2021-10-09 23:35:29 +0530

    removed hash from url

--- food_corner/src/router/index.js
+++ food_corner/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Checkout from "../views/Chckout.vue"
 import orderCreated from "../views/orderCreated.vue"
@@ -47,7 +47,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
